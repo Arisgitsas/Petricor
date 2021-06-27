14941c5e123c7da7022feed18ff2892422ad0ab9
Arisgitsas <32568023+Arisgitsas@users.noreply.github.com>
2021-06-27 17:51:18 +0300

    Delete src directory

--- src/dbcon/Provider.java
+++
@@ -1,6 +0,0 @@
-package dbcon;
-public interface Provider {
-    String username = "postgres";
-    String password = "12345678";
-    String connURL = "jdbc:postgresql://localhost:5432/java2021";
-}
